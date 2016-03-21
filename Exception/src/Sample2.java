a9923e38760bb8bf4cd309e93430e88a392fdcea
MadamGaBi <chesakg@mail.ru>
2016-03-21 14:17:21 +0200

    Exception changes

--- Exception/src/Sample2.java
+++ Exception/src/Sample2.java
@@ -5,6 +5,9 @@ public class Sample2 {
     public static void main(String[] args){
         Sample1 obj = new Sample1();
         try {
+            String a="f d s";
+            String[] r=a.split(" ");
+            System.out.println(r.length);
             obj.myMethod();
         } catch (ArithmeticException ex1) {
             System.out.println("This is ArithmeticException");
@@ -15,5 +18,11 @@ public class Sample2 {
         } catch (Exception ex4) {
             System.out.println("This is Exception");
         }
+        byte b[]=new byte[80];
+        for (int i=0; i<b.length; i++)
+            b[i]=(byte)System.in.read();
+        Integer i=5000;
+        System.out.println(i.hashCode());
     }
+
 }
